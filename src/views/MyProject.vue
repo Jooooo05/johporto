2e27af13914366c93bf89764fac8c4d9439942e5
Jooooo05 <pd12pd13@gmail.com>
2025-01-03 12:41:24 +0700

    menyelesaikan responsive halaman myproject selesai tahap awal untuk porto

--- src/views/MyProject.vue
+++ src/views/MyProject.vue
@@ -11,7 +11,7 @@ import { RouterLink } from 'vue-router';
    <main class="mt-20 border-b pb-3">
     <div class="flex justify-start items-center gap-3">
       <box-icon name='widget' type='solid' color='black' size="lg"></box-icon>
-      <h1 class="poppins-medium tracking-widest text-3xl">MyProject</h1>
+      <h1 class="poppins-medium tracking-widest  text-3xl">MyProject</h1>
     </div>
     <p class="poppins mt-2">The following are some of the projects I have worked on, both private and open source.</p>
    </main>
@@ -25,27 +25,27 @@ import { RouterLink } from 'vue-router';
       <!-- title -->
        <div class="relative pt-5">
 
-        <span class="block w-28 h-1 bg-slate-800 rounded-full absolute top-0 -left-6"></span>
-        <span class="block w-32 h-1 bg-slate-800 mt-1 rounded-full absolute top-2 left-8"></span>
+        <span class="block w-28 h-1 bg-slate-800 rounded-full absolute top-0 md:-left-6 left-2/4"></span>
+        <span class="block w-32 h-1 bg-slate-800 mt-1 rounded-full absolute top-2 md:left-8 left-1/4"></span>
         <div class="">
-          <h3 class="poppins-medium tracking-widest text-2xl">Highlight</h3>
+          <h3 class="poppins-medium tracking-widest text-2xl text-center md:text-start">Highlight</h3>
         </div>
 
        </div>
       <!-- title -->
 
-      <div class="flex mt-16">
+      <div class="flex flex-col md:flex-row gap-[470px] md:gap-0 mt-16">
 
         <!-- left container -->
          <div class="basis-1/2">
           <div class="relative">
-            <div class="w-[500px] hover:scale-125 absolute z-10 top-0 transform transition-transform duration-300">
+            <div class="w-[300px] sm:w-[350px] md:w-[400px] lg:w-[500px] hover:scale-125 absolute z-10 hover:z-30 top-0 right-10 md:right-14 transform transition-transform duration-300">
               <img src="@/assets/img/highlight/1.png" alt="emsaec login" class="w-full grayscale hover:grayscale-0 transition-all ease-in duration-300">
             </div>
-            <div class="w-[500px] hover:scale-125 absolute z-20 top-32 -left-28 transform transition-transform duration-300">
+            <div class="w-[300px] sm:w-[350px] md:w-[400px] lg:w-[500px] hover:scale-125 absolute z-20 top-32 left-6 sm:-left-28 md:-left-8 transform transition-transform duration-300">
               <img src="@/assets/img/highlight/2.png" alt="emsaec dashboard" class="w-full grayscale hover:grayscale-0 transition-all ease-in duration-300 ">
             </div>
-            <div class="w-[500px] hover:scale-125 absolute z-10 top-60 right-10 transform transition-transform duration-300">
+            <div class="w-[300px] sm:w-[350px] md:w-[400px] lg:w-[500px] hover:scale-125 absolute z-10 hover:z-30 top-60 right-10 transform transition-transform duration-300">
               <img src="@/assets/img/highlight/3.png" alt="emsaec modul" class="w-full grayscale hover:grayscale-0 transition-all ease-in duration-300 ">
             </div>
           </div>
@@ -82,14 +82,14 @@ import { RouterLink } from 'vue-router';
 
 
   <!-- recent my project -->
-   <main class="mt-44">
+   <main class="mt-32 md:mt-44">
 
     <!-- title -->
     <div class="relative pt-5">
-      <span class="block w-28 h-1 bg-slate-800 rounded-full absolute top-0 -left-6"></span>
-      <span class="block w-32 h-1 bg-slate-800 mt-1 rounded-full absolute top-2 left-8"></span>
+      <span class="block w-28 h-1 bg-slate-800 rounded-full absolute top-0 md:-left-6 left-2/4"></span>
+      <span class="block w-32 h-1 bg-slate-800 mt-1 rounded-full absolute top-2 md:left-8 left-1/4"></span>
       <div class="">
-        <h3 class="poppins-medium tracking-widest text-2xl">Recent my Project</h3>
+        <h3 class="poppins-medium tracking-widest text-2xl text-center md:text-start">Recent my Project</h3>
       </div>
     </div>
     <!-- title -->
@@ -97,12 +97,12 @@ import { RouterLink } from 'vue-router';
     <!-- recent -->
      <div class="pb-20">
 
-      <div class="grid grid-cols-2 gap-4  mt-10">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4  mt-10">
         <div class="w-full h-[350px] bg-[url('@/assets/img/recentproject/2.png')] bg-cover bg-center relative">
             <span class="absolute top-0 left-0 poppins-medium text-lg tracking-widest p-2 bg-slate-700 text-white ">2024</span>
             <div class="group w-full h-full bg-slate-400 bg-opacity-75 hover:bg-opacity-0 ease-in-out duration-500 flex flex-col justify-center items-center">
               <!-- Konten yang ingin hilang saat hover -->
-              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500">E-msaec Virtual Learning Environment</h1>
+              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500 text-center">E-msaec Virtual Learning Environment</h1>
               <p class="poppins text-slate-900 mt-2 group-hover:opacity-0 transition-opacity duration-500">Lorem</p>
             </div>
         </div>
@@ -110,7 +110,7 @@ import { RouterLink } from 'vue-router';
           <span class="absolute top-0 left-0 poppins-medium text-lg tracking-widest p-2 bg-slate-700 text-white ">2024</span>
             <div class="group w-full h-full bg-slate-400 bg-opacity-75 hover:bg-opacity-0 ease-in-out duration-500 flex flex-col justify-center items-center">
               <!-- Konten yang ingin hilang saat hover -->
-              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500">E-Consultation</h1>
+              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500 text-center">E-Consultation</h1>
               <p class="poppins text-slate-900 mt-2 group-hover:opacity-0 transition-opacity duration-500">Lorem</p>
             </div>
         </div>
@@ -118,7 +118,7 @@ import { RouterLink } from 'vue-router';
           <span class="absolute top-0 left-0 poppins-medium text-lg tracking-widest p-2 bg-slate-700 text-white ">2024</span>
             <div class="group w-full h-full bg-slate-400 bg-opacity-75 hover:bg-opacity-0 ease-in-out duration-500 flex flex-col justify-center items-center">
               <!-- Konten yang ingin hilang saat hover -->
-              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500">Teman Tani</h1>
+              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500 text-center">Teman Tani</h1>
               <p class="poppins text-slate-900 mt-2 group-hover:opacity-0 transition-opacity duration-500">Lorem</p>
             </div>
         </div>
@@ -126,7 +126,7 @@ import { RouterLink } from 'vue-router';
           <span class="absolute top-0 left-0 poppins-medium text-lg tracking-widest p-2 bg-slate-700 text-white ">2024</span>
             <div class="group w-full h-full bg-slate-400 bg-opacity-75 hover:bg-opacity-0 ease-in-out duration-500 flex flex-col justify-center items-center">
               <!-- Konten yang ingin hilang saat hover -->
-              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500">Storry Apps</h1>
+              <h1 class="poppins-semibold text-xl tracking-widest text-slate-900 group-hover:opacity-0 transition-opacity duration-500 text-center">Storry Apps</h1>
               <p class="poppins text-slate-900 mt-2 group-hover:opacity-0 transition-opacity duration-500">Lorem</p>
             </div>
         </div>
@@ -139,11 +139,11 @@ import { RouterLink } from 'vue-router';
   <!-- recent my project -->
 
   <!-- footer -->
-   <main class="mt-32 mb-32">
+   <main class="mt-20 md:mt-32 mb-32 ">
     <div class="flex flex-col justify-center items-center gap-3">
-      <RouterLink to="" class="poppins tracking-widest border-b-2 border-transparent hover:border-current duration-300">Want something like this?</RouterLink>
+      <RouterLink to="" class="poppins text-sm md:text-base tracking-widest border-b-2 border-transparent hover:border-current duration-300">Want something like this?</RouterLink>
       <section class="flex justify-center items-center gap-2">
-        <RouterLink to="" class="poppins-medium text-5xl border-b-2 border-transparent hover:border-current duration-300">Get In Touch</RouterLink>
+        <RouterLink to="" class="poppins-medium text-3xl md:text-5xl border-b-2 border-transparent hover:border-current duration-300">Get In Touch</RouterLink>
         <box-icon name='right-arrow-circle' size="lg"></box-icon>
       </section>
     </div>
