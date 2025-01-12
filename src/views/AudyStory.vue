8c1d6018cdd71cdadd7baa4bfad4052615d9f00d
Jooooo05 <pd12pd13@gmail.com>
2025-01-12 09:15:12 +0700

    lanjutin audystorry

--- src/views/AudyStory.vue
+++ src/views/AudyStory.vue
@@ -86,7 +86,143 @@
 <!-- title story-->
 
 <!-- my story -->
- <main class=""></main>
+ <main class="mt-8 mb-10">
+
+  <!-- story pertama -->
+  <div class="w-full  relative flex items-center justify-center gap-20">
+
+    <div class="w-[3px] h-full bg-zinc-300 absolute top-1/2 left-1/2 transform -translate-y-1/2">
+      <span class="w-7 h-7 absolute -top-3 left-1/2 -translate-x-1/2 rounded-full bg-zinc-300"></span>
+      <div class="w-0 h-0 border-t-[9px] border-b-[9px] border-r-[18px] border-transparent border-r-zinc-300 absolute top-1/2 -translate-y-1/2 -left-4"></div>
+    </div>
+
+    <div class="w-1/2 h-full bg-white shadow-lg p-8 rounded-xl">
+      <h3 class="mochiy tracking-widest text-xl">How we got acquainted</h3>
+      <p class="poppins text-sm tracking-wider mt-1">2 Oktober 11.31</p>
+      <p class="poppins text-sm mt-1">Honestly lupa kapan tepatnya aku dikenalin sama Audy, tapi aku masih inget banget momen awalnya. Hari itu Sabtu, tiba-tiba temenku, Ellen, nelpon. Dia bilang mau ngenalin temennya ke aku. Aku yang waktu itu lagi yahh open juga buat kenalan sama orang baru, langsung kepikiran, "Why not, right?"</p>
+      <p class="poppins text-sm mt-1">Ellen terus ngirimin aku akun Instagram-nya Audy. Dia bilang, "Ini orangnya, Jo. Cobalah kau liat-liat dulu. Gimana? Oke kan orangnya?" Di situ Ellen mulai ngejelasin panjang lebar tentang Audy. Katanya Audy orangnya baik, asik, pokoknya the kind of person you’d want to know lebih jauhlah. Sambil Ellen cerita, aku mulai scroll Instagram-nya Audy dan, I don’t know why, ada sesuatu yang bikin aku tertarik. Tapi, ada rasa ragu juga waktu itu. Di pikiran aku muncul, "Duh, takut nih. Takut kedepannya malah aku yg capek sendiri atau gimana."</p>
+      <p class="poppins text-sm mt-1">tapi si ellen selalu yg nanya nanya gimana jo sudah kau chat belum pokoknya maksa aku buat chatt temennya si audy ini. "Gimana, Jo? Udah kau chat belum?" Sampai akhirnya, karena dia bener-bener push aku, I decided to take that first step.</p>
+      <p class="poppins text-sm mt-1">Dan di tanggal 2 Oktober, jam 11.31, aku ngirim pesan pertama ke Audy. That’s how it all started. 🤗</p>
+    </div>
+
+    <div class="w-1/2 h-full">
+
+      <div class="relative shadow-2xl">
+        <div class="w-[200px] h-[433px] absolute z-30 top-1/2 left-1/3 transform -translate-y-1/2 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/storry1.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[5%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/storry2.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-10 top-1/2 left-[60%] transform -translate-y-1/2 rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/4.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <!-- story pertama -->
+
+  <!-- story kedua -->
+  <div class="w-full relative flex flex-row-reverse items-center justify-center gap-20 ">
+
+    <div class="w-[3px] h-full bg-zinc-300 absolute top-1/2 left-1/2 transform -translate-y-1/2">
+      <div class="w-0 h-0 border-t-[9px] border-b-[9px] border-l-[18px] border-transparent border-l-zinc-300 absolute top-1/2 -translate-y-1/2 -right-4"></div>
+    </div>
+
+    <div class="w-1/2 h-full bg-white shadow-lg p-8 rounded-xl">
+      <h3 class="mochiy tracking-widest text-xl">Konyolnya Gue</h3>
+      <p class="poppins text-sm mt-1">Jadi gini, waktu pertama kali gue ngechat Audy, honestly nggak punya ekspektasi apa-apa. Ya, pokoknya jalanin aja tanpa berharap besar. Tapi, karena ada rasa ragu dan nggak yakin, gue akhirnya cerita ke teman dekatku, Steven. Gue bilang, "Pen, gue dikenalin cewek sama Ellen nih." gue juga cerita kalau gue sebenarnya tertarik, tapi di sisi lain gue punya keraguan besar buat deketin Audy</p>
+      <p class="poppins text-sm mt-1">Ngobrol tuh panjang lebar, sampai dititik si Steven malah bilang, "Jo, gimana kalau lu sama temen gue aja, si Grace?" nah si tolol ini langsung ngechat ke orangnya langsung kyk minta izin buat ngenalin gue ke si Grace. Gue bilang "woe masa gue deketin dua orang sekaligus? kayak apaan tau gue, giu" nah si Tipen kan emang sifatnya kek anjing yah wkwkw malah bilang "Nggak apa-apa, Jo. Namanya juga kenalan, nggak ada yang salah." Akhirnya, yah karena gue juga masih punya keraguan sama Audy, mikirnya "Yaudalah, kenalan aja, kek gada salahnya hehehe peace ✌️"</p>
+      <p class="poppins-semibold text-sm">Dan di titik ini, kekonyolanku dimulai.</p>
+      <p class="poppins text-sm mt-1">Gue sama Steven sama sekali nggak mikir buat car tahu lebih dalam soal circle mereka. Ternyata! tanpa kami duga, Grace sama Audy itu temen dekat gais! hahah mereka sama-sama aktif di organisasi PCM, dan yah sesuai yang kami duga dan takutkan langsung tau dong mereka. Bisa dibilang yah lasung jadi bahan gosipnya mereka</p>
+      <p class="poppins text-sm mt-1">Pas tau hal ini, rasanya... bjirr mati aje udeh. Malunya bener-bener nggak bisa dijelasin pakai kata-kata. Tapi ya, mau nggak mau, gue harus jelasin apa adanya. Akhirnya gue coba cerita jujur gimana bisa itu bisa terjadi aku juga ragu awalnya deketin audy Wahahaha konyol banget co</p>
+    </div>
+
+    <div class="w-1/2 h-full">
+
+      <div class="relative shadow-2xl">
+
+        <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[20%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/storry3.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-10 top-1/2 left-[55%] transform -translate-y-1/2 rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/5.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <!-- story kedua -->
+
+  <!-- story ketiga -->
+  <div class="w-full h-[550px] relative flex items-center justify-center gap-20">
+
+    <div class="w-[3px] h-full bg-zinc-300 absolute top-1/2 left-1/2 transform -translate-y-1/2">
+      <div class="w-0 h-0 border-t-[9px] border-b-[9px] border-r-[18px] border-transparent border-r-zinc-300 absolute top-1/2 -translate-y-1/2 -left-4"></div>
+    </div>
+
+    <div class="w-1/2 h-full flex items-center">
+      <div class="w-full bg-white shadow-lg p-8 rounded-xl">
+        <h3 class="mochiy tracking-widest text-xl">Move to whatshapp</h3>
+        <p class="poppins text-sm tracking-wider mt-1">8 Oktober 14.16</p>
+        <p class="poppins text-sm mt-1">Awal aku ngechat itu dari dm dan setelah coba berbicara lumayan banyak dia bilang "ehh boleee lewat wa ajaa gaa, dm ga enak soalnya jujur wkwkw" yaudah aku lsg jawab boleh banget tuhh wkwkw, untuk part ini ga banyak sih yg bisa di ceritain cuma so happy bisa pindah ke whatshapp hehehe :)</p>
+      </div>
+    </div>
+
+    <div class="w-1/2">
+
+      <div class="relative shadow-2xl">
+        <div class="w-[200px] h-[433px] absolute z-30 top-1/2 left-1/3 transform -translate-y-1/2 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/6.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[5%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/7.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-10 top-1/2 left-[60%] transform -translate-y-1/2 rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/8.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <!-- story ketiga -->
+
+  <!-- story keempat -->
+  <div class="w-full relative flex flex-row-reverse items-center justify-center gap-20 ">
+
+    <div class="w-[3px] h-full bg-zinc-300 absolute top-1/2 left-1/2 transform -translate-y-1/2">
+      <div class="w-0 h-0 border-t-[9px] border-b-[9px] border-l-[18px] border-transparent border-l-zinc-300 absolute top-1/2 -translate-y-1/2 -right-4"></div>
+    </div>
+
+    <div class="w-1/2 h-full bg-white shadow-lg p-8 rounded-xl">
+      <h3 class="mochiy tracking-widest text-xl">Almost gave up</h3>
+      <p class="poppins text-sm tracking-wider mt-1">16 Oktober</p>
+      <p class="poppins text-sm mt-1">Jadi gini, waktu pertama kali gue ngechat Audy, honestly nggak punya ekspektasi apa-apa. Ya, pokoknya jalanin aja tanpa berharap besar. Tapi, karena ada rasa ragu dan nggak yakin, gue akhirnya cerita ke teman dekatku, Steven. Gue bilang, "Pen, gue dikenalin cewek sama Ellen nih." gue juga cerita kalau gue sebenarnya tertarik, tapi di sisi lain gue punya keraguan besar buat deketin Audy</p>
+      <p class="poppins text-sm mt-1">Pas tau hal ini, rasanya... bjirr mati aje udeh. Malunya bener-bener nggak bisa dijelasin pakai kata-kata. Tapi ya, mau nggak mau, gue harus jelasin apa adanya. Akhirnya gue coba cerita jujur gimana bisa itu bisa terjadi aku juga ragu awalnya deketin audy Wahahaha konyol banget co</p>
+    </div>
+
+    <div class="w-1/2 h-full">
+
+      <div class="relative shadow-2xl">
+
+        <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[20%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/9.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-10 top-1/2 left-[55%] transform -translate-y-1/2 rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/10.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <!-- story kedua -->
+
+ </main>
 <!-- my story -->
 
 
