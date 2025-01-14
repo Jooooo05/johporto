5246b08e6cc08069f28880f99b98f32273cc900c
Jooooo05 <pd12pd13@gmail.com>
2025-01-14 13:51:32 +0700

    continue

--- src/views/AudyStory.vue
+++ src/views/AudyStory.vue
@@ -143,7 +143,6 @@
     <div class="w-1/2 h-full">
 
       <div class="relative shadow-2xl">
-
         <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[20%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
           <img src="@/assets/story_audy/mystory/storry3.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
         </div>
@@ -201,18 +200,23 @@
     <div class="w-1/2 h-full bg-white shadow-lg p-8 rounded-xl">
       <h3 class="mochiy tracking-widest text-xl">Almost gave up</h3>
       <p class="poppins text-sm tracking-wider mt-1">16 Oktober</p>
-      <p class="poppins text-sm mt-1">Jadi gini, waktu pertama kali gue ngechat Audy, honestly nggak punya ekspektasi apa-apa. Ya, pokoknya jalanin aja tanpa berharap besar. Tapi, karena ada rasa ragu dan nggak yakin, gue akhirnya cerita ke teman dekatku, Steven. Gue bilang, "Pen, gue dikenalin cewek sama Ellen nih." gue juga cerita kalau gue sebenarnya tertarik, tapi di sisi lain gue punya keraguan besar buat deketin Audy</p>
-      <p class="poppins text-sm mt-1">Pas tau hal ini, rasanya... bjirr mati aje udeh. Malunya bener-bener nggak bisa dijelasin pakai kata-kata. Tapi ya, mau nggak mau, gue harus jelasin apa adanya. Akhirnya gue coba cerita jujur gimana bisa itu bisa terjadi aku juga ragu awalnya deketin audy Wahahaha konyol banget co</p>
+      <p class="poppins text-sm mt-1">Setelah kami kenal satu sama lain selama beberapa hari—honestly, I don’t even know how long exactly—it felt like everything was just <span class="font-semibold">meh</span>. Gimana ya, rasanya stuck banget, kayak kurang ada progress, sampai akhirnya aku mutusin buat cerita ke Ellen. Aku cerita panjang lebar ke dia, bilang kalau aku merasa stuck dan nggak tahu harus gimana. Terus aku bilang ke Ellen, "Lenn, coba deh tanyain ke Audy, sebenarnya dia itu ada interest nggak sih?" Aku juga bilang ke Ellen kalau aku bakal mutusin langkahku berikutnya setelah tahu jawabannya.</p>
+      <p class="poppins text-sm mt-1">Nggak lama setelah itu, Ellen cerita ke Audy. Nah, yang ini aku nggak tahu detail diskusi mereka gimana, tapi aku rada curiga juga sih soal hasil obrolan itu. Tapi yang sampai ke aku katanya, <span class="font-semibold">"Audy sih interest-interest aja."</span></p>
+      <p class="poppins text-sm mt-1">Waktu dengar itu, honestly aku kayak... "Hhmmm...percaya gak yah?" Aku nggak langsung nge-chat Audy hari itu. Seharian aku diem, nggak ada inisiatif buat nyapa dia duluan. Tiba-tiba malamnya, dia nge-chat: <span class="font-semibold">"Hai, apakah sudah tidur?"</span> Aku kaget sih. Habis itu, ya udah, NGILANG!!. ANJG!? iyaa anak anjg emang, fag 🤣</p>
+      <p class="poppins text-sm mt-1">Pas aku tanya kenapa dia cuma nge-chat gitu terus ngilang, kalo klarifikasi dri orangnya sih: "Aku sengaja biar besoknya kita masih punya topik buat ngobrol." Dalam hati aku mikir: "Helehh, bacot bett ni orang." Tapi ya udahlah, aku ketawa aja. Wkwk.</p>
+      <p class="poppins text-sm mt-1">Akhirnya masa PDKT kami berlanjut seperti biasa. Kadang absurd, kadang bikin gemes, tapi ya itulah yang bikin semuanya seru.</p>
     </div>
 
     <div class="w-1/2 h-full">
 
       <div class="relative shadow-2xl">
-
-        <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[20%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+        <div class="w-[200px] h-[433px] absolute z-30 top-1/2 left-1/3 transform -translate-y-1/2 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/11.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[5%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
           <img src="@/assets/story_audy/mystory/9.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
         </div>
-        <div class="w-[200px] h-[433px] absolute z-10 top-1/2 left-[55%] transform -translate-y-1/2 rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+        <div class="w-[200px] h-[433px] absolute z-10 top-1/2 left-[60%] transform -translate-y-1/2 rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
           <img src="@/assets/story_audy/mystory/10.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
         </div>
       </div>
@@ -220,10 +224,85 @@
     </div>
 
   </div>
-  <!-- story kedua -->
+  <!-- story keempat -->
+
+  <!-- storry kelima -->
+  <div class="w-full relative flex items-center justify-center gap-20">
+
+    <div class="w-[3px] h-full bg-zinc-300 absolute top-1/2 left-1/2 transform -translate-y-1/2">
+      <div class="w-0 h-0 border-t-[9px] border-b-[9px] border-r-[18px] border-transparent border-r-zinc-300 absolute top-1/2 -translate-y-1/2 -left-4"></div>
+    </div>
+
+    <div class="w-1/2 h-full flex items-center">
+      <div class="w-full bg-white shadow-lg p-8 rounded-xl">
+        <h3 class="mochiy tracking-widest text-xl">Video Call pertama</h3>
+        <p class="poppins text-sm tracking-wider mt-1">18 Oktober 19.52</p>
+        <p class="poppins text-sm mt-1">Setelah kami mengenal lebih dalam satu sama lain, ada satu momen yang bener-bener aku ingat sampai sekarang. Waktu itu, aku lagi ikut kebaktian Rabu malam di chapel. Tiba-tiba hape aku geter, pas aku cek ternyata ada video call group. Isinya? Ellen dan Audy! Dalam hati aku langsung panik: <span class="font-semibold">"Ancritt! Ngapain mereka?!"</span> Wkwk. Aku nggak jawab call itu, nggak angkat juga. Tapi aku langsung chat Ellen, bilang, "Aku lagi ibadah nih, tunggu dulu ya." Dengan perasaan campur aduk, aku coba fokus lagi ke kebaktian, walaupun agak dagdigdug nggak karuan.</p>
+        <p class="poppins text-sm mt-1">Setelah kebaktian selesai, aku langsung otw ke kamar asrama. Baru aja sampai, Ellen langsung chat: "Udah selesai kah Rabu malamnya? Join yuk ke video call group tadi!" Aku sempet nolak, bilang: "Aduh Lenn, belum siap mental aku, takuttt." Tapi Ellen maksa banget, jadi ya udah, akhirnya aku join juga.</p>
+        <p class="poppins text-sm mt-1">Pas masuk ke call, ternyata ada satu orang lagi yang aku nggak kenal. Katanya namanya Udin, temen dekat mereka pas di Pekanbaru. Jujur, aku diem banget sepanjang call itu, nggak tahu harus ngomong apa. Rasanya awkward parah. Setelah selesai call, aku langsung chat Audy kalau aku emang gugup kalau ngomong sama orang baru—jaim dikit lah istilahnya. Terus Audy cuma jawab santai: <span class="font-semibold">"Iya gapapa kok. Kalau kamu kapan-kapan mau call lagi, it'll be nice!" </span>Aku langsung jawab, "SIAP!" Wkwk.</p>
+        <p class="poppins text-sm mt-1">Abis call itu, aku dan Audy lanjut chat seperti biasa. Kebetulan waktu itu aku lagi sendirian di kamar karena teman-teman asrama lagi pada weekend. Aku cerita ke dia soal itu, dan dia tiba-tiba nanya: <span class="font-semibold">"Eh, berarti kamu lagi sendiri di kamar ya?"</span> Aku nyaut iseng: <span class="font-semibold">"Iya nih, lagi sendiri di kamar :( Temenin aku dong pliss wkwkw."</span> Tapi karena nggak mau keliatan terlalu desperate kalau ditolak, aku buru-buru tambahin: <span class="font-semibold"> "Haha nggak deng, becanda."</span> WAHAHAH.</p>
+        <p class="poppins text-sm mt-1">Eh, ternyata dia jawab: "Mau call nggak? Aku gabut, belum bisa tidur juga." YEAYY! Langsung aja aku call dia, tanpa pikir panjang. itu call sampai jam 1 malem sih kalo ga salah wkwkwk</p>
+      </div>
+    </div>
+
+    <div class="w-1/2">
+
+      <div class="relative shadow-2xl">
+
+        <div class="w-[200px] h-[433px] absolute z-20 top-1/2 left-[20%] -translate-y-1/2 transform -rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/12.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        <div class="w-[200px] h-[433px] absolute z-10 top-1/2 left-[55%] transform -translate-y-1/2 rotate-6 hover:rotate-0 hover:z-40 hover:scale-105 transition-all ease-in duration-200">
+          <img src="@/assets/story_audy/mystory/13.png" alt="storry_we_meet" class="w-full h-full object-cover rounded-xl ">
+        </div>
+        </div>
+
+    </div>
+
+  </div>
+  <!-- storry kelima -->
 
  </main>
 <!-- my story -->
 
+<!-- my butterf -->
+<main class="mt-24">
+  <!-- title -->
+  <div class="relative pt-5">
+    <span class="block w-32 h-1 bg-slate-800 rounded-full absolute left-1/2 -top-2 transform -translate-y-1/2"></span>
+    <span class="block w-36 h-1 bg-slate-800 mt-1 rounded-full absolute top-0 left-[40%]"></span>
+    <div class="flex items-center gap-2 w-fit m-auto">
+      <box-icon name='heart-circle' size="lg"></box-icon>
+      <h1 class="poppins-medium tracking-widest text-3xl">My Butterfly era</h1>
+    </div>
+    <p class="mt-3 poppins text-sm text-center">Setelah call pertama itu, aku ngerasanya kalo ada sesuatu yang beda idk exactly to explain it, but there was this feeling-like..were getting closer hahaha, lebih intens, dan lebih terbuka satu sama lain. Obrolan-obrolan kami jadi lebih mengalir, lebih sering, dan somehow terasa effortless. And this is where my <span class="font-semibold">butterfly era</span> officially began ☺️</p>
+  </div>
+  <!-- title -->
 
+  <!-- container image -->
+   <div class="w-full h-[800px] mt-8 bg-red-300 relative">
+
+    <div class="w-[400px] overflow-hidden rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all ease-in duration-300 cursor-pointer">
+      <img src="@/assets/story_audy/butterfly_era/2.png" alt="selfie_time" class=" transform hover:scale-110 transition-all ease-in duration-200">
+    </div>
+    <div class="w-[400px] overflow-hidden rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all ease-in duration-300 cursor-pointer absolute top-0 right-0">
+      <img src="@/assets/story_audy/butterfly_era/3.png" alt="selfie_time" class=" transform hover:scale-110 transition-all ease-in duration-200">
+    </div>
+    <div class="w-[400px] overflow-hidden rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all ease-in duration-300 cursor-pointer absolute top-0 right-1/2 transform translate-x-1/2">
+      <img src="@/assets/story_audy/butterfly_era/4.png" alt="selfie_time" class=" transform hover:scale-110 transition-all ease-in duration-200">
+    </div>
+    <div class="w-[400px] overflow-hidden rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all ease-in duration-300 cursor-pointer absolute top-2 left-1/2 ">
+      <img src="@/assets/story_audy/butterfly_era/4.png" alt="selfie_time" class=" transform hover:scale-110 transition-all ease-in duration-200">
+    </div>
+    <div class="w-[400px] overflow-hidden rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all ease-in duration-300 cursor-pointer">
+      <img src="@/assets/story_audy/butterfly_era/4.png" alt="selfie_time" class=" transform hover:scale-110 transition-all ease-in duration-200">
+    </div>
+    <div class="w-[400px] overflow-hidden rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all ease-in duration-300 cursor-pointer">
+      <img src="@/assets/story_audy/butterfly_era/4.png" alt="selfie_time" class=" transform hover:scale-110 transition-all ease-in duration-200">
+    </div>
+
+   </div>
+  <!-- container image -->
+
+</main>
 </template>
