5e268b07bb0adfa3c87a1b5afb493e8be67add89
Jooooo05 <pd12pd13@gmail.com>
2025-01-14 14:18:29 +0700

    fix route audystory

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -2,10 +2,19 @@
   import { ref } from 'vue';
 
   const isMenuOpen = ref(false);
+  const isAudyStorryOpen = ref(false);
+  const countOpen = ref(0);
 
   const toggleMenu = () => {
     isMenuOpen.value = !isMenuOpen.value;
   };
+
+  function openAudystorry() {
+    countOpen.value++;
+    if (countOpen.value == 2){
+      isAudyStorryOpen.value = true;
+    }
+  }
   </script>
 
 <template>
@@ -14,9 +23,11 @@
     <main class="flex justify-between items-center ">
 
       <div class="poppins relative z-50">
-        <p class="text-base font-medium">Yonatan's</p>
+        <p @click="openAudystorry" class="text-base font-medium">Yonatan's</p>
         <p class="text-sm">Web Developer</p>
       </div>
+
+      <RouterLink v-if="isAudyStorryOpen" to="/audystory" class="px-5 py-2 bg-pink-600 text-white rounded-lg hover:bg-white hover:border-2 hover:border-pink-600 hover:text-pink-800 transition-all ease-in">Audyy story</RouterLink>
 
       <!-- desktop -->
       <nav class="hidden md:flex gap-12">
